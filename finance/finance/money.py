5052e065db112052dd646d3d4bc970b0789ba198
Travis Robson <travisjrobson@gmail.com>
2020-06-24 20:07:53 -0700

    Finished tests

--- finance/finance/money.py
+++ finance/finance/money.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 
-from decimal import Decimal
+from decimal import Decimal, ROUND_HALF_UP, localcontext
 
 
 class Money:
@@ -34,32 +34,42 @@ class Money:
     
   def __add__(self, rhs):
     if isinstance(rhs, Money):
-      return Money(amount=self._amount + rhs.amount)
+      return Money(amount = self._amount + rhs.amount)
     else:
-      return Money(amount=self._amount + Decimal(rhs))
+      return Money(amount = self._amount + Decimal(rhs))
 
     
   def __sub__(self, rhs):
     if isinstance(rhs, Money):
-      return Money(amount=self._amount - rhs.amount)
+      return Money(amount = self._amount - rhs.amount)
     else:
-      return Money(amount=self._amount - Decimal(rhs))
+      return Money(amount = self._amount - Decimal(rhs))
 
 
-  def __pos__(self, rhs):
-    return Money(amount=self._amount)
+  def __pos__(self):
+    return Money(amount = self._amount)
 
 
-  def __neg__(self, rhs):
-    return Money(amount=-self._amount)
+  def __neg__(self):
+    return Money(amount = -self._amount)
+
+
+  def __abs__(self):
+    return Money(amount = abs(self._amount))
+
+
+  def __round__(self, n):
+    """todo Is this how I wish to round things?"""
+    rounded_amount = self._amount.quantize(Decimal('0.01'), ROUND_HALF_UP)
+    return Money(amount = rounded_amount)
 
 
   def __mul__(self, rhs):
-    return Money(amount=Decimal(rhs)*self._amount) 
+    return Money(amount = Decimal(rhs)*self._amount) 
 
 
   def __truediv__(self, rhs):
-    return Money(amount=self._amount/Decimal(rhs))
+    return Money(amount = self._amount / Decimal(rhs))
 
 
   __radd__ = __add__ # commutative
@@ -75,6 +85,10 @@ class Money:
 
 
   def __le__(self, rhs):
+    if isinstance(rhs, Money):
+      return self._amount >= rhs._amount
+    else:
+      return self >= Money(rhs)   
 
 
   def __gt__(self, rhs):
@@ -85,10 +99,24 @@ class Money:
 
 
   def __ge__(self, rhs):
+    if isinstance(rhs, Money):
+      return self._amount >= rhs._amount
+    else:
+      return self >= Money(rhs)
 
 
   def __eq__(self, rhs):
+    if isinstance(rhs, Money):
+      return self._amount == rhs._amount
+    else:
+      return self == Money(rhs)
 
+
+  def __bool__(self):
+    return self._amount != 0
+
+
+  __nonzero__ = __bool__
 
 
 
--- tests/test_money.py
+++ tests/test_money.py
@@ -9,7 +9,7 @@ from finance.finance.money import Money
 
 class MoneyTestCase(TestCase):
 
-  def test_create_from_int_literal(self):
+  def test_create_from_float(self):
     """
     For user's convenience we expect to be able to construct a Money instance
     from a float or int.
@@ -36,8 +36,71 @@ class MoneyTestCase(TestCase):
     self.assertEqual(result.amount, Decimal("-15.34"))
 
 
-  def test_less_than(self):
+  def test_add(self):
+    a = Money(1.30)
+    b = Money(2.60)
+    self.assertAlmostEqual(a + b, Money(3.90))
+
+
+  def test_sub(self):
+    a = Money(3.45)
+    b = Money(4.60)
+    self.assertAlmostEqual(a - b, Money(-1.15))
+
+
+  def test_neg(self):
+    self.assertAlmostEqual(-Money(3.67), Money(-3.67))
+
+
+  def test_abs(self):
+    self.assertAlmostEqual(Money(1.23), abs(Money(-1.23)))
+
+
+  def test_mul(self):
+    a = Money(3.33)
+    b = 4 * a
+    self.assertAlmostEqual(b, Money(13.32))
+
+
+  def test_div(self):
+    a = Money(3.33)
+    b = a / 3
+    self.assertAlmostEqual(b, Money(1.11))
+
+
+  def test_lt(self):
     one = Money(1)
     two = Money(2)
     self.assertTrue(one < two)
+
+
+  def test_gt(self):
+    one = Money(1)
+    two = Money(2)
+    self.assertTrue(two > one)
+
+
+  def test_eq(self):
+    a = Money(1.45)
+    b = Money(1.45)
+    self.assertEqual(a, b)
+
+
+  def test_bool_true(self):
+    self.assertTrue(Money(1))
+
+
+  def test_bool_false(self):
+    self.assertFalse(Money(0))
+
+
+
+
+
+
+
+
+
+
+
 
